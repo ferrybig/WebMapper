d31029f4c1964e8b58699386269b1179805b825b
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-11-05 12:47:44 +0100

    Added DefaultRequestWrapper
    This class handles the convertion from unknown incoming userdata objects to the correct underlying type

---
+++ src/main/java/me/ferrybig/javacoding/webmapper/requests/DefaultRequestWrapper.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.javacoding.webmapper.requests;
+
+import me.ferrybig.javacoding.webmapper.EndpointResult;
+import me.ferrybig.javacoding.webmapper.Session;
+import static me.ferrybig.javacoding.webmapper.requests.DefaultRequestWrapper.DataType.EMPTY;
+import static me.ferrybig.javacoding.webmapper.requests.DefaultRequestWrapper.DataType.OPTIONAL;
+import static me.ferrybig.javacoding.webmapper.requests.DefaultRequestWrapper.DataType.REQUIRED;
+import io.netty.channel.ChannelHandlerContext;
+import java.nio.charset.Charset;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ *
+ * @author Fernando
+ * @param <T>
+ */
+public abstract class DefaultRequestWrapper<T> implements RequestMapper{
+
+	protected DataType allowUserData = OPTIONAL;
+	protected final Class<T> dataType;
+	
+	public DefaultRequestWrapper(Class<T> dataType) {
+		this.dataType = Objects.requireNonNull(dataType, "dataType == null");
+	}
+
+	public DataType getAllowUserData() {
+		return allowUserData;
+	}
+
+	public void setAllowUserData(DataType allowUserData) {
+		this.allowUserData = allowUserData;
+	}
+
+	public Class<T> getDataType() {
+		return dataType;
+	}
+	
+	@Override
+	public EndpointResult handleHttpRequest(ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) {
+		if(allowUserData == EMPTY && userData.isPresent()) {
+			return new EndpointResult(EndpointResult.Result.SYNTAX_ERROR, "Bad Syntax, bogus userdata", Charset.defaultCharset(), EndpointResult.ContentType.TEXT);
+		}
+		if(allowUserData == REQUIRED && !userData.isPresent()) {
+			return new EndpointResult(EndpointResult.Result.SYNTAX_ERROR, "Bad Syntax, missing userdata", Charset.defaultCharset(), EndpointResult.ContentType.TEXT);
+		}
+		if(userData.isPresent() && !dataType.isAssignableFrom(userData.get().getClass())) {
+			return new EndpointResult(EndpointResult.Result.SYNTAX_ERROR, "Bad Syntax, userdata wrong format", Charset.defaultCharset(), EndpointResult.ContentType.TEXT);
+		}
+		return handle(ctx, endpoint, session, userData.map(dataType::cast));
+	}
+	
+	protected abstract EndpointResult handle(ChannelHandlerContext ctx, String endpoint, Session session, Optional<? super T> userData);
+
+	public static enum DataType {
+		REQUIRED, OPTIONAL, EMPTY,
+	}
+	
+	
+}
---
+++ src/test/java/me/ferrybig/javacoding/webmapper/test/requests/DefaultRequestMapperTest.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.javacoding.webmapper.test.requests;
+
+import me.ferrybig.javacoding.webmapper.EndpointResult;
+import me.ferrybig.javacoding.webmapper.EndpointResult.Result;
+import me.ferrybig.javacoding.webmapper.Session;
+import me.ferrybig.javacoding.webmapper.requests.DefaultRequestWrapper;
+import me.ferrybig.javacoding.webmapper.requests.RequestMapper;
+import io.netty.channel.ChannelHandlerContext;
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.json.JSONObject;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fernando
+ */
+public class DefaultRequestMapperTest {
+
+	private static final EndpointResult RESULT
+			= new EndpointResult(EndpointResult.Result.OK, "", Charset.defaultCharset(), EndpointResult.ContentType.TEXT);
+
+	@Test
+	public void requiredUserDataMissingTest() {
+		DefaultRequestWrapper wrap = new DefaultRequestWrapper(Object.class) {
+
+			@Override
+			public EndpointResult handle(ChannelHandlerContext ctx, String endpoint, Session session, Optional userData) {
+				fail();
+				return null;
+			}
+		};
+		wrap.setAllowUserData(DefaultRequestWrapper.DataType.REQUIRED);
+		assertEquals(wrap.handleHttpRequest(null, null, null, Optional.empty()).getResult(), Result.SYNTAX_ERROR);
+	}
+
+	@Test
+	public void requiredUserDataWorkingTest() {
+		DefaultRequestWrapper wrap = new DefaultRequestWrapper(Object.class) {
+
+			@Override
+			public EndpointResult handle(ChannelHandlerContext ctx, String endpoint, Session session, Optional userData) {
+				return RESULT;
+			}
+		};
+		wrap.setAllowUserData(DefaultRequestWrapper.DataType.REQUIRED);
+		assertEquals(wrap.handleHttpRequest(null, null, null, Optional.of(new Object())).getResult(), Result.OK);
+	}
+
+	@Test
+	public void automaticCastingWorkingTest() {
+		DefaultRequestWrapper wrap = new DefaultRequestWrapper(JSONObject.class) {
+
+			@Override
+			public EndpointResult handle(ChannelHandlerContext ctx, String endpoint, Session session, Optional userData) {
+				return RESULT;
+			}
+		};
+		wrap.setAllowUserData(DefaultRequestWrapper.DataType.REQUIRED);
+		assertEquals(wrap.handleHttpRequest(null, null, null, Optional.of(new JSONObject())).getResult(), Result.OK);
+	}
+
+	@Test
+	public void automaticCastingFailedTest() {
+		DefaultRequestWrapper wrap = new DefaultRequestWrapper(JSONObject.class) {
+
+			@Override
+			public EndpointResult handle(ChannelHandlerContext ctx, String endpoint, Session session, Optional userData) {
+				return RESULT;
+			}
+		};
+		wrap.setAllowUserData(DefaultRequestWrapper.DataType.REQUIRED);
+		assertEquals(wrap.handleHttpRequest(null, null, null, Optional.of(new Object())).getResult(), Result.SYNTAX_ERROR);
+	}
+}
