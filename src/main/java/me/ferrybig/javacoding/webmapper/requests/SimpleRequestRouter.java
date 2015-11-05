c82ee5ada5ad8730bdbb5836fc5201cdacbc4806
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-11-05 12:46:34 +0100

    Added SimpleRequestRouter & tests
    SimpleRequestRouter

---
+++ src/main/java/me/ferrybig/javacoding/webmapper/requests/SimpleRequestRouter.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.javacoding.webmapper.requests;
+
+import me.ferrybig.javacoding.webmapper.EndpointResult;
+import me.ferrybig.javacoding.webmapper.Session;
+import io.netty.channel.ChannelHandlerContext;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.TreeMap;
+
+/**
+ * This class handles routing between RequestMappers and named endpoints.
+ * @author Fernando
+ */
+public class SimpleRequestRouter implements RequestMapper {
+
+	private final Map<String, RequestMapper> routes;
+	private final RequestMapper defaultRoute;
+
+	public SimpleRequestRouter(RequestMapper defaultRoute) {
+		this.defaultRoute = Objects.requireNonNull(defaultRoute, "defaultRoute == null");
+		this.routes = new TreeMap<>();
+	}
+
+	public void addRoute(String endpoint, RequestMapper route) {
+		this.addRoute(endpoint, route, true);
+	}
+
+	public void addRoute(String endpoint, RequestMapper route, boolean useSubString) {
+		if (useSubString) {
+			final RequestMapper route2 = Objects.requireNonNull(route, "route == null");
+			final int endpointLength = endpoint.length();
+			route = (ChannelHandlerContext ctx, String endpoint1, Session session, Optional<?> userData) -> route2.handleHttpRequest(ctx, endpoint1.substring(endpointLength), session, userData);
+		}
+		this.routes.put(endpoint, route);
+	}
+
+	public Optional<RequestMapper> getRouteExact(String route) {
+		return Optional.ofNullable(this.routes.get(route));
+	}
+
+	public Optional<RequestMapper> getRoute(String route) {
+		RequestMapper r = null;
+		int lastLength = 0;
+		for (Map.Entry<String, RequestMapper> localRoute : this.routes.entrySet()) {
+			int localRouteLength = localRoute.getKey().length();
+			if (route.startsWith(localRoute.getKey()) && lastLength < localRouteLength) {
+				lastLength = localRouteLength;
+				r = localRoute.getValue();
+				if (localRouteLength == route.length()) {
+					return Optional.of(localRoute.getValue());
+				}
+			}
+		}
+		return Optional.ofNullable(r);
+	}
+
+	public boolean removeRoute(String route) {
+		return this.routes.remove(route) != null;
+	}
+
+	private RequestMapper resolvRoute(String route) {
+		return this.getRoute(route).orElse(defaultRoute);
+	}
+
+	@Override
+	public EndpointResult handleHttpRequest(ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) {
+		return resolvRoute(endpoint).handleHttpRequest(ctx, endpoint, session, userData);
+	}
+}
---
+++ src/test/java/me/ferrybig/javacoding/webmapper/test/requests/SimpleRequestRouterTest.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.javacoding.webmapper.test.requests;
+
+import me.ferrybig.javacoding.webmapper.EndpointResult;
+import me.ferrybig.javacoding.webmapper.Session;
+import me.ferrybig.javacoding.webmapper.requests.RequestMapper;
+import me.ferrybig.javacoding.webmapper.requests.SimpleRequestRouter;
+import io.netty.channel.ChannelHandlerContext;
+import java.nio.charset.Charset;
+import java.util.Optional;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Fernando
+ */
+public class SimpleRequestRouterTest {
+
+	private static final EndpointResult RESULT
+			= new EndpointResult(EndpointResult.Result.OK, "", Charset.defaultCharset(), EndpointResult.ContentType.TEXT);
+	private static final RequestMapper SUCCESS = (ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) -> {
+		return RESULT;
+	};
+	private static final RequestMapper FAILING = (ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) -> {
+		fail();
+		throw new AssertionError();
+	};
+
+	@Test
+	public void exactMatchTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", SUCCESS);
+		assertEquals(mapper.handleHttpRequest(null, "/test", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void weakMatchTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", SUCCESS);
+		assertEquals(mapper.handleHttpRequest(null, "/test/1", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void longestRouteTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", FAILING);
+		mapper.addRoute("/test/1", SUCCESS);
+		assertEquals(mapper.handleHttpRequest(null, "/test/1/2", null, Optional.empty()), RESULT);
+
+		mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test/1", SUCCESS);
+		mapper.addRoute("/test", FAILING);
+		assertEquals(mapper.handleHttpRequest(null, "/test/1/2", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void takesDefaultRouteTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(SUCCESS);
+		mapper.addRoute("/test", FAILING);
+		mapper.addRoute("/test/1", FAILING);
+		mapper.addRoute("/1/2", FAILING);
+		assertEquals(mapper.handleHttpRequest(null, "/blah/1/2", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void substringWorksTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", (ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) -> {
+			assertEquals(endpoint.length(), "".length());
+			return RESULT;
+		});
+		assertEquals(mapper.handleHttpRequest(null, "/test", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void substringWorksNonExactMatchTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", (ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) -> {
+			assertEquals(endpoint.length(), "/ttt".length());
+			return RESULT;
+		});
+		assertEquals(mapper.handleHttpRequest(null, "/test/ttt", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void substringCanBeDisabledTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", (ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) -> {
+			assertEquals(endpoint.length(), "/test".length());
+			return RESULT;
+		}, false);
+		assertEquals(mapper.handleHttpRequest(null, "/test", null, Optional.empty()), RESULT);
+	}
+
+	@Test
+	public void substringCanBeDisabledNonExactMatchTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(FAILING);
+		mapper.addRoute("/test", (ChannelHandlerContext ctx, String endpoint, Session session, Optional<?> userData) -> {
+			assertEquals(endpoint.length(), "/test/ttt".length());
+			return RESULT;
+		}, false);
+		assertEquals(mapper.handleHttpRequest(null, "/test/ttt", null, Optional.empty()), RESULT);
+	}
+	
+	@Test
+	public void routeCanBeRemovedTest() {
+		SimpleRequestRouter mapper = new SimpleRequestRouter(SUCCESS);
+		mapper.addRoute("/test", FAILING);
+		mapper.removeRoute("/test");
+		assertEquals(mapper.handleHttpRequest(null, "/test", null, Optional.empty()), RESULT);
+	}
+}
