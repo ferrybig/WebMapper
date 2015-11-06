a0ce8cc0798bbd56166b7de7ab5e4ef8c1f0ba65
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-11-06 12:38:05 +0100

    Added NotFoundRoute, a route that returns always NotFound

---
+++ src/main/java/me/ferrybig/javacoding/webmapper/requests/routes/NotFoundRoute.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.ferrybig.javacoding.webmapper.requests.routes;
+
+import me.ferrybig.javacoding.webmapper.EndpointResult;
+import me.ferrybig.javacoding.webmapper.Session;
+import me.ferrybig.javacoding.webmapper.requests.DefaultRequestWrapper;
+import io.netty.channel.ChannelHandlerContext;
+import java.util.Optional;
+
+/**
+ *
+ * @author Fernando
+ */
+public class NotFoundRoute extends DefaultRequestWrapper<Object> {
+
+	public NotFoundRoute() {
+		super(Object.class);
+		this.allowUserData = DataType.OPTIONAL;
+	}
+	
+	@Override
+	protected EndpointResult handle(ChannelHandlerContext ctx, String endpoint, Session session, Optional<? super Object> userData) {
+		return new EndpointResult(EndpointResult.Result.UNKNOWN_ENDPOINT, "", EndpointResult.ContentType.TEXT);
+	}
+	
+}
