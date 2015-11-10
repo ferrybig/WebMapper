a5e9080c03430835fcd577aa03fe3a1df18a26ce
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-11-10 13:43:13 +0100

    [fbb] Fixed broken build, added StreamException

---
+++ src/main/java/me/ferrybig/javacoding/webmapper/exceptions/StreamException.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package me.ferrybig.javacoding.webmapper.exceptions;
+
+/**
+ *
+ * @author Fernando
+ */
+public class StreamException extends RuntimeException {
+
+    /**
+     * Creates a new instance of <code>StreamException</code> without detail message.
+     */
+    public StreamException() {
+        super();
+    }
+
+
+    /**
+     * Constructs an instance of <code>StreamException</code> with the specified detail message.
+     * @param msg the detail message.
+     */
+    public StreamException(String msg) {
+        super(msg);
+    }
+
+    /**
+	 * Constructs an instance of <code>StreamException</code> with the specified
+	 * detail message and cause.
+	 *
+	 * @param message the detail message.
+	 * @param cause the cause
+	 */
+	public StreamException(String message, Throwable cause) {
+		super(message);
+        this.initCause(cause);
+	}
+
+	/**
+	 * Constructs an instance of <code>StreamException</code> with the specified
+	 * cause.
+	 *
+	 * @param cause the cause
+	 */
+	public StreamException(Throwable cause) {
+		this(cause.toString());
+        this.initCause(cause);
+	}
+}
