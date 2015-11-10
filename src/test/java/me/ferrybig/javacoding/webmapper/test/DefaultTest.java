af8118f90466f5d4da8ff575c60a64bf011f7e27
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-11-10 13:41:40 +0100

    Changed DefaultTest to test sane build conditions

--- src/test/java/me/ferrybig/javacoding/webmapper/test/DefaultTest.java
+++ src/test/java/me/ferrybig/javacoding/webmapper/test/DefaultTest.java
@@ -1,5 +1,6 @@
 package me.ferrybig.javacoding.webmapper.test;
 
+import java.nio.charset.Charset;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -8,5 +9,8 @@ import org.junit.Test;
  * @author Fernando
  */
 public class DefaultTest {
-	
+	@Test
+	public void defaultCharsetTests() {
+		assertTrue(Charset.isSupported("UTF-8"));
+	}
 }
