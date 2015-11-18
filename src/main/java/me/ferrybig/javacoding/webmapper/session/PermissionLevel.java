df842c3dc68f7b8c6d87a191615b018f55606d39
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-11-18 12:07:15 +0100

    Fixed ExceptionInInitializerError in PermissionLevel
    http://stackoverflow.com/q/33777646/1542723

--- src/main/java/me/ferrybig/javacoding/webmapper/session/PermissionLevel.java
+++ src/main/java/me/ferrybig/javacoding/webmapper/session/PermissionLevel.java
@@ -6,9 +6,11 @@
 package me.ferrybig.javacoding.webmapper.session;
 
 import static me.ferrybig.javacoding.webmapper.util.StreamUtil.concat;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -17,63 +19,73 @@ import java.util.stream.Collectors;
  * @author Fernando
  */
 public enum PermissionLevel {
+
 	/**
 	 * Users that are making their first request to the api
 	 */
-	EVERYONE, 
+	EVERYONE,
 	/**
 	 * Users that have their session cookie set, but aren't logged in
 	 */
-	ANONYMOUS(EVERYONE), 
+	ANONYMOUS(EVERYONE),
 	/**
 	 * Users that are logged in, but haven't verified their email
 	 */
-	NEW_USER(ANONYMOUS), 
+	NEW_USER(ANONYMOUS),
 	/**
 	 * Normal registered users
 	 */
-	REGISTERED_USER(NEW_USER), 
+	REGISTERED_USER(NEW_USER),
 	/**
 	 * Users with the edit role
 	 */
-	CONTENT_EDITOR(REGISTERED_USER), 
+	CONTENT_EDITOR(REGISTERED_USER),
 	/**
 	 * Admin users
 	 */
-	ADMIN(CONTENT_EDITOR),
-	;
-	private final Set<PermissionLevel> parents;
-	private final Set<PermissionLevel> fullParents;
-	private final Set<PermissionLevel> fullParentAndMe;
-	
+	ADMIN(CONTENT_EDITOR),;
+	private Set<PermissionLevel> parents;
+	private Set<PermissionLevel> fullParents;
+	private Set<PermissionLevel> fullParentAndMe;
+
 	@SuppressWarnings("LeakingThisInConstructor")
 	private PermissionLevel() {
 		this.fullParents = this.parents = Collections.emptySet();
-		this.fullParentAndMe = Collections.unmodifiableSet(EnumSet.of(this));
+		this.fullParentAndMe = Collections.singleton(this);
 	}
-	
+
 	@SuppressWarnings("LeakingThisInConstructor")
-	private PermissionLevel(PermissionLevel parent1,PermissionLevel ... parents) {
-		this.parents = Collections.unmodifiableSet(EnumSet.of(parent1, parents));
+	private PermissionLevel(PermissionLevel... parents) {
+		this.parents = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(parents)));
 		this.fullParents = Collections.unmodifiableSet(concat(this.parents.stream(),
 				this.parents.stream().map(PermissionLevel::getFullParents).flatMap(Collection::stream)).
-				collect(Collectors.toCollection(()->EnumSet.noneOf(PermissionLevel.class))));
-		EnumSet<PermissionLevel> tmp = EnumSet.of(this);
-		tmp.addAll(this.fullParents);
+				collect(Collectors.toCollection(HashSet::new)));
+		Set<PermissionLevel> tmp = new HashSet<>(fullParents);
+		tmp.add(this);
 		this.fullParentAndMe = Collections.unmodifiableSet(tmp);
 	}
-	
+
+	static {
+		for (PermissionLevel v : values()) {
+			v.parents = Collections.unmodifiableSet(v.parents.isEmpty()
+					? EnumSet.noneOf(PermissionLevel.class) : EnumSet.copyOf(v.parents));
+			v.fullParents = Collections.unmodifiableSet(v.fullParents.isEmpty()
+					? EnumSet.noneOf(PermissionLevel.class) : EnumSet.copyOf(v.fullParents));
+			v.fullParentAndMe = Collections.unmodifiableSet(v.fullParentAndMe.isEmpty()
+					? EnumSet.noneOf(PermissionLevel.class) : EnumSet.copyOf(v.fullParentAndMe));
+		}
+	}
+
 	public Set<PermissionLevel> getParents() {
 		return parents;
 	}
-	
+
 	public Set<PermissionLevel> getFullParents() {
 		return fullParents;
 	}
-	
+
 	public Set<PermissionLevel> getFullParentsAndMe() {
 		return fullParentAndMe;
 	}
-	
-	
+
 }
