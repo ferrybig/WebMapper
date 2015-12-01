de0ff0b4d06fc3c79289cd0c33660f3eb46c35d4
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-12-01 16:56:53 +0100

    Added more tests & pass EventLoopGroups to the tests

--- src/test/java/me/ferrybig/javacoding/webmapper/test/MainServerTest.java
+++ src/test/java/me/ferrybig/javacoding/webmapper/test/MainServerTest.java
@@ -12,22 +12,65 @@ import me.ferrybig.javacoding.webmapper.session.DefaultPermissionsManager;
 import me.ferrybig.javacoding.webmapper.session.DefaultSessionManager;
 import me.ferrybig.javacoding.webmapper.session.PermissionManager;
 import me.ferrybig.javacoding.webmapper.session.SessionManager;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
 import java.io.File;
+import java.util.function.Function;
+import org.junit.AfterClass;
 import static org.junit.Assert.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
 /**
  *
  * @author Fernando
  */
 public class MainServerTest {
+
+	private static EventLoopGroup eventGroup;
+	
+	@BeforeClass
+	public static void init() {
+		eventGroup = new NioEventLoopGroup(1);
+	}
+	
+	@AfterClass
+	public static void shutdown() {
+		eventGroup.shutdownGracefully();
+	}
+	
 	@Test
 	public void canReadSSLCertTest() throws ListenerException, ServerException {
 		PermissionManager perm = new DefaultPermissionsManager();
 		SessionManager ses = new DefaultSessionManager(perm);
-		try (MainServer s = new MainServer(ses, perm, r->null)) {
-			s.addListener("127.24.54.230", 59684, 
-					new File(getClass().getResource("unsafe01.key").getFile()), 
+		try (MainServer s = new MainServer(eventGroup, eventGroup, ses, perm, r -> null)) {
+			s.addListener("127.24.54.230", 59684,
+					new File(getClass().getResource("unsafe01.key").getFile()),
 					new File(getClass().getResource("unsafe01.cert").getFile()));
 		}
 	}
+
+	@Test
+	public void serverCanBeClosedWithoutListenersActiveTest() throws ListenerException, ServerException {
+		PermissionManager perm = new DefaultPermissionsManager();
+		SessionManager ses = new DefaultSessionManager(perm);
+		try (MainServer s = new MainServer(eventGroup, eventGroup, ses, perm, r -> null)) {
+			// Void call to prevent compiler optimalization
+			Function.identity().apply(s);
+		}
+	}
+
+	@Test
+	public void getListenersReturnsListenersTest() throws ListenerException, ServerException {
+		PermissionManager perm = new DefaultPermissionsManager();
+		SessionManager ses = new DefaultSessionManager(perm);
+		try (MainServer s = new MainServer(eventGroup, eventGroup, ses, perm, r -> null)) {
+			s.addListener("127.24.45.230", 59684);
+			s.addListener("127.25.45.230", 59684);
+			s.addListener("127.26.45.230", 59684);
+			s.addListener("127.27.45.230", 59684);
+			assertEquals(4, s.getListeners().size());
+		}
+	}
+
 }
