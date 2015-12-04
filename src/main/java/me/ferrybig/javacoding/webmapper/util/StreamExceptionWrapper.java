138510628f6edd7f8fac1bc5af81b6020097edfc
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-12-04 12:27:16 +0100

    Expanded StreamExceptionWrapper

--- src/main/java/me/ferrybig/javacoding/webmapper/util/StreamExceptionWrapper.java
+++ src/main/java/me/ferrybig/javacoding/webmapper/util/StreamExceptionWrapper.java
@@ -6,6 +6,8 @@
 package me.ferrybig.javacoding.webmapper.util;
 
 import me.ferrybig.javacoding.webmapper.exceptions.StreamException;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -15,41 +17,95 @@ import java.util.function.Supplier;
  */
 public class StreamExceptionWrapper {
 
-	public static <T, U> Function<T, U>
-			wrapEx(ExceptionFunction<T, U, ?> f) {
-		return (T i) -> {
-			try {
-				return f.apply(i);
-			} catch (Exception ex) {
-				if (ex instanceof RuntimeException) {
-					throw (RuntimeException) ex;
+	public static <T, U> Function<T, U> wrapEx(ExceptionFunction<T, U, ?> f) {
+		return new Function<T, U>() {
+
+			@Override
+			public U apply(T i) {
+				try {
+					return f.apply(i);
+				} catch (Exception ex) {
+					if (ex instanceof RuntimeException) {
+						throw (RuntimeException) ex;
+					}
+					throw new StreamException(ex);
 				}
-				throw new StreamException(ex);
 			}
 		};
 	}
 
-	public static <R, T extends Throwable> R unwrap(Supplier<R> r, Class<? extends T> ex) throws T {
-		try {
-			return r.get();
-		} catch (StreamException ex1) {
-			Throwable upstream = ex1.getCause();
-			if (ex.isAssignableFrom(upstream.getClass())) {
-				throw ex.cast(upstream);
+	public static <T, S, U> BiFunction<T, S, U> wrapEx(ExceptionBiFunction<T, S, U, ?> f) {
+		return new BiFunction<T, S, U>() {
+
+			@Override
+			public U apply(T i, S s) {
+				try {
+					return f.apply(i, s);
+				} catch (Exception ex) {
+					if (ex instanceof RuntimeException) {
+						throw (RuntimeException) ex;
+					}
+					throw new StreamException(ex);
+				}
 			}
-			throw ex1;
-		}
+		};
 	}
 
+	public static <U> Supplier<U> wrapEx(ExceptionSupplier<U, ?> f) {
+		return new Supplier<U>() {
+
+			@Override
+			public U get() {
+				try {
+					return f.get();
+				} catch (Exception ex) {
+					if (ex instanceof RuntimeException) {
+						throw (RuntimeException) ex;
+					}
+					throw new StreamException(ex);
+				}
+			}
+		};
+	}
+
+	public static <U> Consumer<U> wrapEx(ExceptionConsumer<U, ?> f) {
+		return new Consumer<U>() {
+
+			@Override
+			public void accept(U u) {
+				try {
+					f.accept(u);
+				} catch (Exception ex) {
+					if (ex instanceof RuntimeException) {
+						throw (RuntimeException) ex;
+					}
+					throw new StreamException(ex);
+				}
+			}
+		};
+	}
+
+	@FunctionalInterface
 	public interface ExceptionFunction<T, R, E extends Exception> {
 
-		/**
-		 * Applies this function to the given argument.
-		 *
-		 * @param t the function argument
-		 * @return the function result
-		 * @throws E the checked exception
-		 */
 		R apply(T t) throws E;
+	}
+
+	@FunctionalInterface
+	public interface ExceptionConsumer<T, E extends Exception> {
+
+		void accept(T t) throws E;
+	}
+
+	@FunctionalInterface
+	public interface ExceptionBiFunction<T, S, R, E extends Exception> {
+
+		R apply(T t, S s) throws E;
+	}
+
+	@FunctionalInterface
+	public interface ExceptionSupplier<R, E extends Exception> {
+
+		R get() throws E;
 	}
 }
